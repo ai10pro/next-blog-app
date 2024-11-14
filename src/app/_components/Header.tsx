076f4cfaf28afa6ffd0fffd4c7d2c15e1a5df36b
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-11-14 10:16:32 +0900

    update Header.tsx and page.tsx

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,9 +1,14 @@
 "use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFish } from "@fortawesome/free-solid-svg-icons";
 
 const Header: React.FC = () => {
   return (
     <header>
-      <div className="bg-slate-800 py-2 font-bold text-white">Header</div>
+      <div className="bg-slate-800 py-2 font-bold text-white">
+        <FontAwesomeIcon icon={faFish} className="mr-1" />
+        Header
+      </div>
     </header>
   );
 };
