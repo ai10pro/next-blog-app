096d934f8a13c83dcb4daa494edd9566141984ab
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-11-14 10:23:13 +0900

    twmergeの追加

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,13 +1,25 @@
 "use client";
+import { twMerge } from "tailwind-merge";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFish } from "@fortawesome/free-solid-svg-icons";
 
 const Header: React.FC = () => {
   return (
     <header>
-      <div className="bg-slate-800 py-2 font-bold text-white">
-        <FontAwesomeIcon icon={faFish} className="mr-1" />
-        Header
+      <div className="bg-slate-800 py-2">
+        <div
+          className={twMerge(
+            "mx-4 max-w-2xl md:mx-auto",
+            "flex items-center justify-between",
+            "text-lg font-bold text-white"
+          )}
+        >
+          <div>
+            <FontAwesomeIcon icon={faFish} className="mr-1" />
+            ai10proのブログ
+          </div>
+          <div>About</div>
+        </div>
       </div>
     </header>
   );
