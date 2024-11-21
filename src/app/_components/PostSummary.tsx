1ab693021be316825de8106ad392e56e9c15b125
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-11-21 09:35:01 +0900

    update PostSummary.tsx

--- src/app/_components/PostSummary.tsx
+++ src/app/_components/PostSummary.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { Post } from "@/app/_types/Post";
+import Link from "next/link";
 
 type Props = {
   post: Post;
@@ -9,8 +10,13 @@ const PostSummary: React.FC<Props> = (props) => {
   const { post } = props;
   return (
     <div className="border border-slate-400 p-3">
-      <div className="font-bold">{post.title}</div>
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      <Link href={`/posts/${post.id}`}>
+        <div className="font-bold">{post.title}</div>
+        <div
+          className="line-clamp-3"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </Link>
     </div>
   );
 };
