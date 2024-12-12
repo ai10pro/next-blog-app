c3af330215f74d69e113a98dfdc3f830b79f0fa8
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-12-12 10:27:56 +0900

    update page.tsx and page.tsx

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import { useParams } from "next/navigation"; // ◀ 注目
 
 import type { Post } from "@/app/_types/Post";
-import dummyPosts from "@/app/_mocks/dummyPosts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
@@ -13,40 +12,52 @@ import DOMPurify from "isomorphic-dompurify";
 // 投稿記事の詳細表示 /posts/[id]
 const Page: React.FC = () => {
   const [post, setPost] = useState<Post | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [fetchError, setFetchError] = useState<string | null>(null);
 
   // 動的ルートパラメータから 記事id を取得 （URL:/posts/[id]）
   const { id } = useParams() as { id: string };
 
+  const apiBaseEp = process.env.NEXT_PUBLIC_MICROCMS_BASE_EP!;
+  const apiKey = process.env.NEXT_PUBLIC_MICROCMS_API_KEY!;
   // コンポーネントが読み込まれたときに「1回だけ」実行する処理
   useEffect(() => {
-    // 本来はウェブAPIを叩いてデータを取得するが、まずはモックデータを使用
-    // (ネットからのデータ取得をシミュレートして１秒後にデータをセットする)
-    setIsLoading(true);
-    const timer = setTimeout(() => {
-      console.log("ウェブAPIからデータを取得しました (虚言)");
-      // dummyPosts から id に一致する投稿を取得してセット
-      setPost(dummyPosts.find((post) => post.id === id) || null);
-      setIsLoading(false);
-    }, 1000);
-
-    // データ取得の途中でページ遷移したときにタイマーを解除する処理
-    return () => clearTimeout(timer);
-  }, [id]);
+    const fetchPosts = async () => {
+      try {
+        // microCMS から記事データを取得
+        const requestUrl = `${apiBaseEp}/posts/${id}/`;
+        const response = await fetch(requestUrl, {
+          method: "GET",
+          cache: "no-store",
+          headers: {
+            "X-MICROCMS-API-KEY": apiKey,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("データの取得に失敗しました");
+        }
+        const data = await response.json();
+        setPost(data as Post);
+      } catch (e) {
+        setFetchError(
+          e instanceof Error ? e.message : "予期せぬエラーが発生しました"
+        );
+      }
+    };
+    fetchPosts();
+  }, [apiBaseEp, apiKey, id]);
 
   // 投稿データの取得中は「Loading...」を表示
-  if (isLoading) {
+  if (fetchError) {
+    return <div>{fetchError}</div>;
+  }
+
+  if (!post) {
     return (
       <div className="text-gray-500">
         <FontAwesomeIcon icon={faSpinner} className="mr-1 animate-spin" />
         Loading...
       </div>
     );
-  }
-
-  // 投稿データが取得できなかったらエラーメッセージを表示
-  if (!post) {
-    return <div>指定idの投稿の取得に失敗しました。</div>;
   }
 
   // HTMLコンテンツのサニタイズ
