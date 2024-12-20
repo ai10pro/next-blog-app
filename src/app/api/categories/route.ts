20b20e8b9fb8fd084f230f19b090c451478d4e86
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-12-20 15:58:33 +0900

    update route.ts GETステータスの作成

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,15 +1,44 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import { Category } from "@prisma/client";
 
 // [GET] /api/categories カテゴリ一覧の取得
 export const GET = async (req: NextRequest) => {
   try {
     const categories = await prisma.category.findMany({
+      select: {
+        id: true,
+        name: true,
+        createdAt: true,
+        updatedAt: true,
+
+        // カテゴリに紐づく投稿を取得する場合
+        // posts: {
+        //   select: {
+        //     post: {
+        //       select: {
+        //         id: true,
+        //         title: true,
+        //         content: true,
+        //         createdAt: true,
+        //       },
+        //     },
+        //   },
+        // },
+
+        // カテゴリに紐づく投稿数を取得する場合
+        // _count: {
+        //   select: {
+        //     posts: true,
+        //   },
+        // },
+      },
       orderBy: {
         createdAt: "desc", // 降順 (新しい順)
       },
     });
-    return NextResponse.json(categories);
+    console.log(categories);
+    return NextResponse.json(categories, { status: 200 }); // 200: OK
   } catch (error) {
     console.error(error);
     return NextResponse.json(
