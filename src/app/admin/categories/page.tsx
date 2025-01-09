e727b9fe3a5c389fd6476707e04fcd35cc4c0cc5
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-01-10 00:54:25 +0900

    admin/categoriesの仮作成

---
+++ src/app/admin/categories/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+const Page: React.FC = () => {
+  return (
+    <main>
+      <div className="mb-2 text-2xl font-bold">カテゴリ一覧管理</div>
+    </main>
+  );
+};
+
+export default Page;
