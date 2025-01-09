bf6ccc73a2402538b7b63d96c519e54f1d68ce72
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-01-10 00:54:46 +0900

    admin/posts/[id]の仮作成

---
+++ src/app/admin/posts/[id]/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+const Page: React.FC = () => {
+  return (
+    <main>
+      <div className="mb-2 text-2xl font-bold">
+        投稿記事の編集（削除を含む）
+      </div>
+    </main>
+  );
+};
+
+export default Page;
